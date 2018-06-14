a6144a093490d14b03ede31d6e8c2f057899f148
DivyaKharbanda <divyakharbanda28@gmail.com>
2018-06-14 16:04:57 -0700

    Done

---
+++ myArrayList/src/myArrayList/util/FileProcessor.java
@@ -0,0 +1,37 @@
+package myArrayList.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import myArrayList.MyArrayList;
+
+public class FileProcessor {
+	public void readLine(MyArrayList myArrayListInstance, String filename) throws IOException
+    {
+		// Here we are reading the lines of file one by one
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferReader = new BufferedReader(fileReader);
+        String readLine = null;
+        try
+        {
+            while((readLine = bufferReader.readLine()) != null)
+            {
+                if((Integer.parseInt(readLine)>=0 && Integer.parseInt(readLine)<=10000))
+                {
+                    myArrayListInstance.insertSorted(Integer.parseInt(readLine));
+                }
+            }
+        }
+        catch(IOException io)
+        {
+            io.printStackTrace();
+        }
+        finally
+        {
+            fileReader.close();
+            bufferReader.close();
+        }
+    }
+}
---
+++ myArrayList/src/myArrayList/util/StdoutDisplayInterface.java
@@ -0,0 +1,7 @@
+package myArrayList.util;
+
+//This is an interfcae, its methods will be implemented by the class which uses this interface
+public interface StdoutDisplayInterface 
+{
+	void writeToStdout(String testName);
+}
